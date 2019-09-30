8cf07c236b00c2d567351e52dc4abbf83b31d869
jschepens <jeremy.schepens@orange.fr>
2019-09-30 12:32:47 +0200

    switch

--- memory.js
+++ memory.js
@@ -1,15 +1,37 @@
 var tablElement = document.createElement("table");
 document.body.appendChild(tablElement);
+var cpt = 1
+nb = 4
+var numbers = [];
+for (var i = 1; i <= nb*nb;i++){
+    numbers.push(i)
+}
 
-var tdList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var generate = document.getElementsByTagName("input")[0];
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < numbers.length; i++) {
+    var randomIndex = Math.floor(Math.random()*(i+1));
+    var temp = numbers[i];
+    numbers[i] = numbers[randomIndex];
+    numbers[randomIndex] = temp;
+}
+
+cpt = 0;
+
+for (let i = 0; i < nb; i++) {
     var trElement = document.createElement("tr");
     tablElement.appendChild(trElement);
-    for (let i = 0; i < 3; i++){
+    for (let j = 0; j < nb; j++){
         var casElement = document.createElement("td");
+        casElement.className = "cell hidden"
         trElement.appendChild(casElement);
-        casElement.innerHTML = tdList[i];
+        casElement.innerHTML = numbers[cpt++];
+        // casElement.addEventListener("click", function(){
+        //     this.classList.remove("hidden"); 
+        //     if (numbers[i] == numbers[i]) {
+        //         casElement.style.backgroundColor = "blue"
+        //     }
+        // })
     }
 }
 
